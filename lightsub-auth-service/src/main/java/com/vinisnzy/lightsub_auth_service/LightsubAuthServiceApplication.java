16eac03ee1e6ff7de11214ea81b928e5081902d3
vinisnzy <manoseilacara123@gmail.com>
2025-12-29 19:21:05 -0300

    feat: create project for auth-service

---
+++ lightsub-auth-service/src/main/java/com/vinisnzy/lightsub_auth_service/LightsubAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.lightsub_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightsubAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightsubAuthServiceApplication.class, args);
+	}
+
+}
---
+++ lightsub-auth-service/src/test/java/com/vinisnzy/lightsub_auth_service/LightsubAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.lightsub_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightsubAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
