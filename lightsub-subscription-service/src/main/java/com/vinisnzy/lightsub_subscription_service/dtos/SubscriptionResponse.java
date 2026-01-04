3f788897701c69d25659e63a3240e7d8ccafa236
vinisnzy <manoseilacara123@gmail.com>
2026-01-04 13:04:22 -0300

    refactor: change IDs fields to UUID and create method fromEntity

--- lightsub-subscription-service/src/main/java/com/vinisnzy/lightsub_subscription_service/dtos/SubscriptionResponse.java
+++ lightsub-subscription-service/src/main/java/com/vinisnzy/lightsub_subscription_service/dtos/SubscriptionResponse.java
@@ -2,15 +2,27 @@ package com.vinisnzy.lightsub_subscription_service.dtos;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
+import com.vinisnzy.lightsub_subscription_service.model.Subscription;
 import com.vinisnzy.lightsub_subscription_service.model.dtos.BillingPeriod;
 
 public record SubscriptionResponse(
-    String id,
-    String userId,
-    String name,
-    BigDecimal price,
-    BillingPeriod billingPeriod,
-    LocalDate renewalDate,
-    String category) {
+        UUID id,
+        UUID userId,
+        String name,
+        BigDecimal price,
+        BillingPeriod billingPeriod,
+        LocalDate renewalDate,
+        String category) {
+    public static SubscriptionResponse fromEntity(Subscription entity) {
+        return new SubscriptionResponse(
+                entity.getId(),
+                entity.getUserId(),
+                entity.getName(),
+                entity.getPrice(),
+                entity.getBillingPeriod(),
+                entity.getRenewalDate(),
+                entity.getCategory());
+    }
 }
