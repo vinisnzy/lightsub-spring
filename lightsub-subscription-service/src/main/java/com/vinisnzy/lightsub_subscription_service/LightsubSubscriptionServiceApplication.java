fc11c568cca021600e23e22ac49ecdb919bfdc8c
vinisnzy <manoseilacara123@gmail.com>
2026-01-04 12:27:39 -0300

    feat: create initial project for subscription service

---
+++ lightsub-subscription-service/src/main/java/com/vinisnzy/lightsub_subscription_service/LightsubSubscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.lightsub_subscription_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightsubSubscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightsubSubscriptionServiceApplication.class, args);
+	}
+
+}
