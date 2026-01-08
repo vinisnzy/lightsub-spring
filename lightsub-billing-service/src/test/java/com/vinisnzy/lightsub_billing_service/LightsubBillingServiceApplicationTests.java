7a342f583f8f6ff01de7317bae7235b781f895f7
Vinícius de Paula <manoseilacara123@gmail.com>
2026-01-08 10:27:41 -0300

    feat: create initial structure for billing service

---
+++ lightsub-billing-service/src/test/java/com/vinisnzy/lightsub_billing_service/LightsubBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.lightsub_billing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightsubBillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
